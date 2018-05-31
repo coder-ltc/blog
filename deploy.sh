d1a9aa4c14953f3ee88c4a26f2d18d5184288b13
coder-ltc <994234517@qq.com>
2018-05-31 23:34:06 +0800

    “update”

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 hexo generate
-cp -R public/* deploy/coder-ltc.github.io
-cd deploy/coder-ltc.github.io
+cp -R public/* ../deploy/coder-ltc.github.io
+cd ../deploy/coder-ltc.github.io
 git add .
 git commit -m “update”
 git push origin master
